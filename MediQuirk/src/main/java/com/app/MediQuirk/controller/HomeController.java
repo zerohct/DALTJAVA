c465452768a81d39973a6fa6451fb98915eb06a3
zerohct <tuonghutnh@gmail.com>
2024-06-11 22:28:07 +0700

    add admin sidebar, form home, securityconfig

--- MediQuirk/src/main/java/com/app/MediQuirk/controller/HomeController.java
+++ MediQuirk/src/main/java/com/app/MediQuirk/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.app.MediQuirk.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String Admin(){
+
+        return "User/index";
+    }
 }
